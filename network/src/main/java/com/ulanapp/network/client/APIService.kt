248dcd9cb5ee70267da4213317d70842fb18dabb
Great <uulnaerkinbaev@gmail.com>
2021-04-25 11:09:30 +0300

    Добавил Path AccessToken

--- network/src/main/java/com/ulanapp/network/client/APIService.kt
+++ network/src/main/java/com/ulanapp/network/client/APIService.kt
@@ -4,13 +4,14 @@ import com.ulanapp.network.responses.AccessTokenResponse
 import com.ulanapp.network.responses.BonusInfoResponse
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface APIService {
 
     companion object {
         private const val ACCESS_TOKEN = "api/v3/clients/accesstoken"
-        private const val GENERAL_INFO = "/api/v3/ibonus/generalinfo"
+        private const val GENERAL_INFO = "/api/v3/ibonus/generalinfo/{AccessToken}"
     }
 
     // метод для логина
@@ -19,5 +20,5 @@ interface APIService {
 
     // метод для получения списка платежей
     @GET(GENERAL_INFO)
-    suspend fun getGeneralInfo(@Query("AccessToken") accessToken: String): BonusInfoResponse
+    suspend fun getGeneralInfo(@Path("AccessToken") accessToken: String): BonusInfoResponse
 }
